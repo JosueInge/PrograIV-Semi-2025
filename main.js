a97cbabf6d17aa496ee1583fed4e9bfd69351236
josueorellana <jm5482017.com>
2025-02-25 13:41:17 -0300

    Funcionamiento

--- main.js
+++ main.js
@@ -3,17 +3,21 @@ const { createApp, ref } = Vue;
 const app = createApp({
     components: {
         alumno,
+        buscaralumno,
         materia,
         matriculaAlumno,
-        inscripcionMaterias
+        inscripcionMaterias,
+        buscarmateria
     },
     data() {
         return {
             forms: {
                 alumno: { mostrar: false },
+                buscarAlumno: {mostrar: false},
                 materia: { mostrar: false },
                 docente: { mostrar: false },
                 matriculaAlumno: { mostrar: false },
+                buscarMateria: {mostrar: false},
                 inscripcionMaterias: { mostrar: false }
             }
         };
