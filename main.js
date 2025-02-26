da737556c48139c8dbaed7f6f2e48363932c7892
josueorellana <jm5482017.com>
2025-02-26 15:33:59 -0300

    cambios

---
+++ main.js
@@ -0,0 +1,40 @@
+const { createApp, ref } = Vue;
+const Dexie = window.Dexie,
+    db = new Dexie('db_codigo_estudiante');
+
+const app = createApp({
+    components: {
+        autor,
+        buscarautor,
+        libro,
+        buscarlibro,
+    },
+    data() {
+        return {
+            forms: {
+                autor: { mostrar: false },
+                buscarautor: { mostrar: false },
+                libro: { mostrar: false },
+                buscarlibro: { mostrar: false },
+            },
+        };
+    },
+    methods: {
+        buscar(form, metodo) {
+            this.$refs[form][metodo]();
+        },
+        abrirFormulario(componente) {
+            this.forms[componente].mostrar = !this.forms[componente].mostrar;
+        },
+        modificar(form, metodo, datos) {
+            this.$refs[form][metodo](datos);
+        }
+    },
+    created() {
+        db.version(1).stores({
+            autores: '++idAutor, codigo, nombre, pais, telefono',
+            libros: '++idLibro, idAutor, lsbn, titulo,editorial,edicion',
+        });
+    }
+});
+app.mount('#app');
